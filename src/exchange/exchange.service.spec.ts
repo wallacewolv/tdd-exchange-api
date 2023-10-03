b1501a5e8951a8aafa97fc6063a56d7961be5450
wallace <wallace_wolv@hotmail.com>
2023-10-02 23:51:30 -0300

    test: Ensure convertAmount called getCurrency twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Amount, ExchangeService } from './exchange.service';
+import { Amount, CurrenciesService, ExchangeService } from './exchange.service';
 import { BadRequestException } from '@nestjs/common';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currenciesServiceMock = { getCurrency: jest.fn() };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock },
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -28,6 +35,12 @@ describe('ExchangeService', () => {
     it('should be not throw if called with valid params', async () => {
       const value: Amount = { from: 'USD', to: 'BRL', amount: '1' };
       await expect(service.convertAmount(value)).resolves.not.toThrow();
+    });
+
+    it('should be called getCurrency twice', async () => {
+      const value: Amount = { from: 'USD', to: 'BRL', amount: '1' };
+      await service.convertAmount(value);
+      await expect(currenciesService.getCurrency).toHaveBeenCalledTimes(2);
     });
   });
 });
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -6,11 +6,22 @@ export interface Amount {
   amount: string;
 }
 
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {
+    return currency;
+  }
+}
+
 @Injectable()
 export class ExchangeService {
+  constructor(private currenciesService: CurrenciesService) {}
+
   async convertAmount({ from, to, amount }: Amount): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
+
+    const currencyForm = this.currenciesService.getCurrency(from);
+    const currencyTo = this.currenciesService.getCurrency(to);
   }
 }

5d9b79a46b71e92643a85eff8e95936c71463a34
wallace <wallace_wolv@hotmail.com>
2023-10-07 19:38:01 -0300

    feat: Create currencies-input type

--- src/currencies/currencies.repository.ts
+++ src/currencies/currencies.repository.ts
@@ -1,6 +1,8 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Currencies } from "./currencies.entity";
-import { InternalServerErrorException } from "@nestjs/common";
+import { InternalServerErrorException } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+
+import { Currencies } from './currencies.entity';
+import { CurrenciesInputType } from './types/currencies-input.type';
 
 @EntityRepository(Currencies)
 export class CurrenciesRepository extends Repository<Currencies> {
@@ -14,11 +16,11 @@ export class CurrenciesRepository extends Repository<Currencies> {
     return result;
   }
 
-  async createCurrency({ currency, value }): Promise<Currencies> {
+  async createCurrency({ currency, value }: CurrenciesInputType): Promise<Currencies> {
     return new Currencies();
   }
 
-  async updateCurrency({ currency, value }): Promise<Currencies> {
+  async updateCurrency({ currency, value }: CurrenciesInputType): Promise<Currencies> {
     return new Currencies();
   }
 
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -2,23 +2,24 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 
 import { Currencies } from './currencies.entity';
 import { CurrenciesRepository } from './currencies.repository';
+import { CurrenciesInputType } from './types/currencies-input.type';
 
 @Injectable()
 export class CurrenciesService {
-  constructor(private currenciesRepository: CurrenciesRepository) {}
+  constructor(private currenciesRepository: CurrenciesRepository) { }
 
   async getCurrency(currency: string): Promise<Currencies> {
     return await this.currenciesRepository.getCurrency(currency);
   }
 
-  async createCurrency({ currency, value }): Promise<Currencies> {
+  async createCurrency({ currency, value }: CurrenciesInputType): Promise<Currencies> {
     if (value <= 0) {
       throw new BadRequestException('The value must be greatest zero.');
     }
     return await this.currenciesRepository.createCurrency({ currency, value });
   }
 
-  async updateCurrency({ currency, value }): Promise<Currencies> {
+  async updateCurrency({ currency, value }: CurrenciesInputType): Promise<Currencies> {
     if (value <= 0) {
       throw new BadRequestException('The value must be greatest zero.');
     }

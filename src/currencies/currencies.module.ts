6b088b74a149f27098693919a21575886e6b8535
wallace <wallace_wolv@hotmail.com>
2023-10-14 20:39:58 -0300

    feat: Create CurrenciesController

--- src/currencies/currencies.module.ts
+++ src/currencies/currencies.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CurrenciesService } from './currencies.service';
+import { CurrenciesController } from './currencies.controller';
 
 @Module({
-  providers: [CurrenciesService]
+  providers: [CurrenciesService],
+  controllers: [CurrenciesController]
 })
 export class CurrenciesModule {}

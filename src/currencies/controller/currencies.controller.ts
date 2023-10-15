5c7defcff21a941df68959a7d07eba1f5732a4d8
wallace <wallace_wolv@hotmail.com>
2023-10-14 22:28:19 -0300

    test: Create method getCurrency in currenciesController and testing

--- src/currencies/controller/currencies.controller.ts
+++ src/currencies/controller/currencies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Currencies } from '../entity/currencies.entity';
+import { CurrenciesService } from '../service/currencies.service';
 
 @Controller('currencies')
-export class CurrenciesController {}
+export class CurrenciesController {
+  constructor(private currenciesService: CurrenciesService) { }
+
+  @Get('/:currency')
+  async getCurrency(@Param('currency') currency: string): Promise<Currencies> {
+    return await this.currenciesService.getCurrency(currency);
+  }
+}
+
